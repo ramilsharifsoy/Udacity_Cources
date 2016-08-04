87fd4300939adbd13c3052eeef4bfe9c91029b05
Ramil Sharifsoy <ramilsharifsoy@users.noreply.github.com>
2016-08-03 20:31:37 -0500

    refactor: add sense and move sections

--- 2_AI_Robotics_Localization.py
+++ 2_AI_Robotics_Localization.py
@@ -1,6 +1,22 @@
+#Reference 1D sense and move functions developed during the lesson:
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i]) 
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
 
-
-
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
 
 # test 1
 colors = [['G', 'G', 'G'],
